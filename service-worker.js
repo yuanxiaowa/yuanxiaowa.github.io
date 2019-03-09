1d5b80773a986cffee276d373cc67098c28857e1
欧顶银 <oudingyin@oudingyindeMacBook-Pro.local>
2019-03-09 12:34:41 +0800

    .

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e2ed2318efb858be8bf894ea1dec8af1.js"
+  "/precache-manifest.e3c523ec274f3ecb05339d8ceb20d7cf.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "front-tool"});
